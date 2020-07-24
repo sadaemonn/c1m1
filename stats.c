426b040a66851b7c574cc201b9e0f23685ec7fcf
Eren Yazici <erennnyazici@gmail.com>
2020-07-24 23:07:37 +0300

    Completing code to the first working version

--- stats.c
+++ stats.c
@@ -42,29 +42,74 @@ void main() {
 }
 
 void print_statistics(unsigned char array[], unsigned int length){
-
+	printf("\n-------Statistics of the array-------\n");
+        printf("\nMinimum: %d", find_minimum(array, length));
+        printf("\nMaximum: %d", find_maximum(array, length));
+        printf("\nMean: %d", find_mean(array, length));
+        printf("\nMedian: %d\n\n", find_median(array, length));
 }
 
 void print_array(unsigned char array[], unsigned int length){
-
+	int i;
+        printf("\n-------The array------- \n\n");
+        for( i=0 ; i<length ; i++ ){
+                printf("%u ", array[i]);
+        }
+        printf("\n");
 }
 
 unsigned char find_median(unsigned char array[], unsigned int length){
-
+	int median;
+        if (length%2==0){
+                median = (array[length/2-1]+array[length/2])/2;
+                return median;
+        }
+        median = array[(length+1)/2-1];
+        return median;
 }
 
 unsigned char find_mean(unsigned char array[], unsigned int length){
+	int i;
+        int sum=0;
+        for( i=0 ; i<length ; i++ ){
+                sum = sum+array[i];
+        }
 
+        return sum/length;
 }
 
 unsigned char find_maximum(unsigned char array[], unsigned int length){
-
+	int i;
+        unsigned char max=0;
+        for( i=0 ; i<length ; i++ ){
+                if ( array[i]>=max ){
+                        max = array[i];
+                }
+        }
+        return max;
 }
 
 unsigned char find_minimum(unsigned char array[], unsigned int length){
-
+	int i;
+        unsigned char min=array[0];
+        for( i=0 ; i<length ; i++ ){
+                if ( array[i]<=min ){
+                        min = array[i];
+                }
+        }
+        return min;
 }
 
 void sort_array(unsigned char array[], unsigned int length){
-
+	unsigned char swap;
+        int a, b;
+        for( a=0 ; a<length ; a++ ){
+                for( b=0 ; b<length-a-1 ; b++){
+                        if( array[b]<array[b+1] ){
+                                swap = array[b];
+                                array[b] = array[b+1];
+                                array[b+1] = swap;
+                        }
+                }
+        }
 }
